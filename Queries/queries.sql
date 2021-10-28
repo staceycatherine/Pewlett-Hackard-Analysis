7664a45080fb396bba32eb771ca052dcc0d3667e
Staceycatherine <staceyhart1002@gmail.com>
2021-10-28 09:16:07 -0400

    Adding Updates and Queries

---
+++ Queries/queries.sql
@@ -0,0 +1,22 @@
+--Retirement Eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Get the number(count) of employees retiring
+SELECT COUNT (first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Create a new table with retirement information
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Check the table
+SELECT * FROM retirement_info;
--- schema.sql
+++ schema.sql
@@ -1,12 +1,22 @@
 --schema.sql
 
 -- Creating tables for PH-EmployeeDB
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS salaries;
+
+-- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
 	dept_no VARCHAR(4) NOT NULL,
 	dept_name VARCHAR(40) NOT NULL,
 	PRIMARY KEY (dept_no),
 	UNIQUE (dept_name)
 ); 
+
 
 CREATE TABLE employees(
 	emp_no INT NOT NULL,
@@ -18,41 +28,48 @@ CREATE TABLE employees(
 	PRIMARY KEY (emp_no)
 );
 
+
 CREATE TABLE dept_manager(
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE salaries(
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-);
+CREATE TABLE dept_emp (    emp_no INT NOT NULL,   
+					   dept_no VARCHAR(4) NOT NULL,    
+					   from_date DATE NOT NULL,   
+					   to_date DATE NOT NULL,    
+					   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),    
+					   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),    
+					   PRIMARY KEY (emp_no, dept_no))
 
-CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no VARCHAR(40) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no)	
-);
 
-CREATE TABLE titles(
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-	);
-	
-SELECT * FROM departments;
+
+CREATE TABLE titles (    emp_no INT NOT NULL,   
+					 title VARCHAR(50) NOT NULL,    
+					 from_date DATE NOT NULL,    
+					 to_date DATE,    
+					 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),   
+					 PRIMARY KEY (emp_no, title, from_date));
+
+
+
+CREATE TABLE salaries (    emp_no INT NOT NULL,    
+					   salary INT NOT NULL,    
+					   from_date DATE NOT NULL,    
+					   to_date DATE NOT NULL,    
+					   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),   
+					   PRIMARY KEY (emp_no, from_date));
+
+
+
+SELECT * FROM departments
+SELECT * FROM employees
+SELECT * FROM dept_manager
+SELECT * FROM dept_emp
+SELECT * FROM titles
+SELECT * FROM salaries
